523d539bdfc1344a59aa7c196682f69be1e8413c
jasminvucelj <jasminvucelj1@gmail.com>
2017-07-28 15:52:54 +0200

    added service to send activity recognition data to main activity

--- app/src/main/java/com/activityrecognitiontest/ActivityRecognitionService.java
+++ app/src/main/java/com/activityrecognitiontest/ActivityRecognitionService.java
@@ -3,6 +3,7 @@ package com.activityrecognitiontest;
 import android.app.IntentService;
 import android.content.Intent;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import java.util.List;
 
@@ -12,13 +13,13 @@ import com.google.android.gms.location.DetectedActivity;
 
 
 
-public class ActivityRecognizedService extends IntentService {
+public class ActivityRecognitionService extends IntentService {
 
-    public ActivityRecognizedService() {
-        super("ActivityRecognizedService");
+    public ActivityRecognitionService() {
+        super("ActivityRecognitionService");
     }
 
-    public ActivityRecognizedService(String name) {
+    public ActivityRecognitionService(String name) {
         super(name);
     }
 
@@ -26,46 +27,54 @@ public class ActivityRecognizedService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         if(ActivityRecognitionResult.hasResult(intent)) {
             ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+            DetectedActivity mostProbableActivity = result.getMostProbableActivity();
+            sendToMainActivity(mostProbableActivity.getType(),
+                    mostProbableActivity.getConfidence());
             // handleDetectedActivities(result.getProbableActivities());
-
-            Log.e("ActivityRecogition",
-                    "Most probable activity: " + result.getMostProbableActivity().toString());
-
         }
     }
+
+
+    private void sendToMainActivity(int type, int confidence) {
+        Intent intent = new Intent("activityRecognitionIntent");
+        intent.putExtra("type", type);
+        intent.putExtra("confidence", confidence);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+
 
     private void handleDetectedActivities(List<DetectedActivity> probableActivities) {
         for(DetectedActivity activity : probableActivities) {
             switch(activity.getType()) {
-                case DetectedActivity.IN_VEHICLE: {
+                case DetectedActivity.IN_VEHICLE: { // 0
                     Log.e("ActivityRecogition", "In Vehicle: " + activity.getConfidence());
                     break;
                 }
-                case DetectedActivity.ON_BICYCLE: {
-                    Log.e("ActivityRecogition", "On Bicycle: " + activity.getConfidence() );
+                case DetectedActivity.ON_BICYCLE: { // 1
+                    Log.e("ActivityRecogition", "On Bicycle: " + activity.getConfidence());
                     break;
                 }
-                case DetectedActivity.ON_FOOT: {
+                case DetectedActivity.ON_FOOT: { // 2
                     Log.e("ActivityRecogition", "On Foot: " + activity.getConfidence());
                     break;
                 }
-                case DetectedActivity.RUNNING: {
+                case DetectedActivity.RUNNING: { // 8
                     Log.e("ActivityRecogition", "Running: " + activity.getConfidence());
                     break;
                 }
-                case DetectedActivity.STILL: {
+                case DetectedActivity.STILL: { // 3
                     Log.e("ActivityRecogition", "Still: " + activity.getConfidence());
                     break;
                 }
-                case DetectedActivity.TILTING: {
+                case DetectedActivity.TILTING: { // 5
                     Log.e("ActivityRecogition", "Tilting: " + activity.getConfidence());
                     break;
                 }
-                case DetectedActivity.WALKING: {
+                case DetectedActivity.WALKING: { // 7
                     Log.e("ActivityRecogition", "Walking: " + activity.getConfidence());
                     break;
                 }
-                case DetectedActivity.UNKNOWN: {
+                case DetectedActivity.UNKNOWN: { // 4
                     Log.e("ActivityRecogition", "Unknown: " + activity.getConfidence());
                     break;
                 }
--- app/src/main/java/com/activityrecognitiontest/MainActivity.java
+++ app/src/main/java/com/activityrecognitiontest/MainActivity.java
@@ -2,27 +2,56 @@ package com.activityrecognitiontest;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.DetectedActivity;
 
 
 public class MainActivity extends Activity implements ConnectionCallbacks, OnConnectionFailedListener {
 
+    final int CONFIDENCE_THRESHOLD = 75;
+
+    TextView tv;
+
     GoogleApiClient mApiClient;
+
+    private BroadcastReceiver activityReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int type = intent.getIntExtra("type", DetectedActivity.UNKNOWN);
+            int confidence = intent.getIntExtra("confidence", 0);
+
+            if(confidence >= CONFIDENCE_THRESHOLD){
+                tv.setText("most probable activity: " + String.valueOf(type));
+            }
+
+            // Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.tv);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                activityReceiver, new IntentFilter("activityRecognitionIntent"));
 
         mApiClient = new GoogleApiClient.Builder(this)
                 .addApi(ActivityRecognition.API)
@@ -34,16 +63,17 @@ public class MainActivity extends Activity implements ConnectionCallbacks, OnCon
 
     }
 
+
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        Intent intent = new Intent(this, ActivityRecognizedService.class);
+        Intent intent = new Intent(this, ActivityRecognitionService.class);
         PendingIntent pendingIntent = PendingIntent.getService(this,
                 0,
                 intent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
         ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mApiClient,
-                5 * 1000,
-                pendingIntent); // 5s
+                3 * 1000,
+                pendingIntent); // 3s
         Log.e("MainActivity","Connected");
     }
 
